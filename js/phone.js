bd0fa7372e697a97ff12acc9fecc5d4f62dd4ab1
MujahidEmon <mujahidulislam.official@gmail.com>
2024-07-11 16:04:50 +0600

    fixing show all btn

--- js/phone.js
+++ js/phone.js
@@ -2,21 +2,32 @@ const loadPhone = async (searchInputText) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInputText}`)
     const data = await res.json();
     const phones = data.data;
+
     displayPhones(phones);
 }
 
 const displayPhones = phones => {
     // console.log(phones)
-
+    
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = ''
+
+    const showAll = document.getElementById('show-all-container');
+
+    if(phones.length > 15){
+        showAll.classList.remove('hidden');
+    }
+    else{
+        showAll.classList.add('hidden');
+    }
+    phones = phones.slice(0,15);
     phones.forEach(phone =>{
         console.log(phone);
 
         // 1. Creating a div
 
         const phoneCard = document.createElement('div')
-        phoneCard.classList = 'card bg-base-100 p-3  shadow-xl'
+        phoneCard.classList = 'card bg-base-100 p-3 mt-5  shadow-xl'
         phoneCard.innerHTML = `
             <figure>
                       <img
@@ -30,6 +41,8 @@ const displayPhones = phones => {
                 <button class="btn btn-primary">Buy Now</button>
                 </div>
             </div>
+
+            
         
         `
         phoneContainer.appendChild(phoneCard)
